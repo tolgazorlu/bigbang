f7399fc0aef7ba920590eaeed19724c7858ceace
tolgazorlu <tolgazorlu17@gmail.com>
2023-09-19 15:10:01 +0300

    sidebar implemented for dashboard

--- client/src/pages/DashboardUsers.tsx
+++ client/src/pages/DashboardUsers.tsx
@@ -7,9 +7,12 @@ import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import { ToastContainer } from "react-toastify";
 import { useGetUsersQuery } from "../hooks/userHooks";
+import Sidebar from "../components/Sidebar";
 
 const DashboardUsers = () => {
   const { data: users, isLoading, error } = useGetUsersQuery();
+
+  console.log(users);
 
   return (
     <>
@@ -29,110 +32,7 @@ const DashboardUsers = () => {
         <title>Dashboard</title>
       </Helmet>
       <div className="bg-white grid grid-cols-12">
-        <aside
-          id="default-sidebar"
-          className="border-r-2 col-span-3"
-          aria-label="Sidebar"
-        >
-          <div className="h-full px-3 py-4">
-            <ul className="space-y-2 font-medium">
-              <li>
-                <a
-                  href="/dashboard"
-                  className="flex items-center p-2 text-gray-900 rounded-lg "
-                >
-                  <svg
-                    className="w-5 h-5 text-gray-500 transition duration-75 "
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 22 21"
-                  >
-                    <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                    <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                  </svg>
-                  <span className="ml-3">Dashboard</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 text-gray-900 rounded-lg "
-                >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 18 18"
-                  >
-                    <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                  </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-                  <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800">
-                    Pro
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center p-2 text-gray-900 rounded-lg "
-                >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 "
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
-                  </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-                  <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full ">
-                    3
-                  </span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/dashboard/users"
-                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 18"
-                  >
-                    <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                  </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/dashboard/products"
-                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 18 20"
-                  >
-                    <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
-                  </svg>
-                  <span className="flex-1 ml-3 whitespace-nowrap">
-                    Products
-                  </span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </aside>
+        <Sidebar />
 
         <div className="p-4 col-span-9">
           <ul>
@@ -157,13 +57,13 @@ const DashboardUsers = () => {
                         Name
                       </th>
                       <th scope="col" className="px-6 py-3">
-                        Category
+                        email
                       </th>
                       <th scope="col" className="px-6 py-3">
-                        Rating
+                        Phone Number
                       </th>
                       <th scope="col" className="px-6 py-3">
-                        Price
+                        Account Date
                       </th>
                       <th scope="col" className="px-6 py-3">
                         Action
@@ -182,6 +82,10 @@ const DashboardUsers = () => {
                             {user.lastName}
                           </th>
                           <td className="px-6 py-4">{user.email}</td>
+                          <td className="px-6 py-4">{user.phoneNumber}</td>
+                          <td className="px-6 py-4">
+                            {user.createdAt.substring(0, 10)}
+                          </td>
                           <td className="px-6 py-4 flex gap-2">
                             <a
                               href="#"
