e909659d13d0947c7e7b0b6995d80917a1f9e098
tolgazorlu <tolgazorlu17@gmail.com>
2023-09-20 08:35:01 +0300

    user del model removed

--- client/src/pages/DashboardUsers.tsx
+++ client/src/pages/DashboardUsers.tsx
@@ -9,17 +9,18 @@ import Sidebar from "../components/Sidebar";
 
 const DashboardUsers = () => {
   const { data: users, isLoading, error, refetch } = useGetUsersQuery();
-  const {mutateAsync: deleteUser, isLoading: loadingDelete} = useDeleteUserMutation()
+  const { mutateAsync: deleteUser, isLoading: loadingDelete } =
+    useDeleteUserMutation();
 
   const userDeleteHandler = async (id: string) => {
     try {
-      await deleteUser(id)
-      refetch()
-      toast.success('User deleted successfully')
+      await deleteUser(id);
+      refetch();
+      toast.success("User deleted successfully");
     } catch (error) {
-      toast.error(getError(error as ApiError))
+      toast.error(getError(error as ApiError));
     }
-  }
+  };
 
   return (
     <>
@@ -43,7 +44,7 @@ const DashboardUsers = () => {
 
         <div className="p-4 col-span-9">
           <ul>
-          {loadingDelete && <Loading></Loading>}
+            {loadingDelete && <Loading></Loading>}
             {isLoading ? (
               <Loading />
             ) : error ? (
@@ -93,63 +94,10 @@ const DashboardUsers = () => {
                           <td className="px-6 py-4">
                             {user.createdAt.substring(0, 10)}
                           </td>
-                          <td className="px-6 py-4 flex gap-2">
-                            <button onClick={() => userDeleteHandler(user._id)}>
-                              delete
+                          <td className="px-6 py-4 flex">
+                            <button className="py-2 px-3 rounded-md text-red-500 hover:bg-gray-100 hover:text-red-700 font-poppins" onClick={() => userDeleteHandler(user._id)}>
+                              Delete
                             </button>
-
-                            {/* MODAL */}
-
-                            <button
-                              className="block text-red-600 hover:text-white hover:bg-red-600 font-medium rounded-lg text-sm px-4 py-1.5 text-center"
-                              type="button"
-                              onClick={()=> document.getElementById('my_modal_1').showModal()}
-                            >
-                              Remove
-                            </button>
-                            <dialog
-                              id="my_modal_1"
-                              className="modal"
-                            >
-                              <div className="relative w-full max-w-md max-h-full">
-                                <form className="relative bg-white rounded-lg shadow dialog">
-                                  <div className="p-6 text-center">
-                                    <svg
-                                      className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
-                                      aria-hidden="true"
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      fill="none"
-                                      viewBox="0 0 20 20"
-                                    >
-                                      <path
-                                        stroke="currentColor"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                                      />
-                                    </svg>
-                                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                                      Are you sure you want to delete this
-                                      user?
-                                    </h3>
-                                    <button
-                                      type="button"
-                                      onClick={() => {userDeleteHandler(user._id)}}
-                                      className="text-white bg-red-600 hover:bg-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-                                    >
-                                      Yes, I'm sure
-                                    </button>
-                                    <button
-                                      className="text-gray-500 bg-white hover:bg-gray-100 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 focus:z-10"
-                                    >
-                                      No, cancel
-                                    </button>
-                                  </div>
-                                </form>
-                              </div>
-                            </dialog>
-
                           </td>
                         </tr>
                       );
